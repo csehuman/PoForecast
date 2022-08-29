fcf40117707528334f209c7a1f45ec7ff4e2605e
csehuman <paullee20204@snu.ac.kr>
2022-08-29 16:24:27 +0700

    Step 13 - Show current weather in table view

---
+++ PoForecast/Formatter/Double+Formatter.swift
@@ -0,0 +1,23 @@
+//
+//  Double+Formatter.swift
+//  PoForecast
+//
+//  Created by Paul Lee on 2022/08/29.
+//
+
+import Foundation
+
+fileprivate let temperatureFormatter: MeasurementFormatter = {
+    let f = MeasurementFormatter()
+    f.locale = Locale(identifier: "ko_kr")
+    f.numberFormatter.maximumFractionDigits = 1
+    f.unitOptions = .temperatureWithoutUnit
+    return f
+}()
+
+extension Double {
+    var temperatureString: String {
+        let temp = Measurement<UnitTemperature>(value: self, unit: .celsius)
+        return temperatureFormatter.string(from: temp)
+    }
+}
