dbe1422335e760e71020e662e66a04fac09b411a
csehuman <paullee20204@snu.ac.kr>
2022-08-29 11:34:27 +0700

    Step 7 - Connect cell outlet

---
+++ PoForecast/Cell/ForecastTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  ForecastTableViewCell.swift
+//  PoForecast
+//
+//  Created by Paul Lee on 2022/08/29.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+    
+    static let identifier = "ForecastTableViewCell"
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
